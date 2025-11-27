bffc7fa4ce23362b56010030291f03a212887a21
Gulyaev Martinas <killfind@yandex.ru>
2025-11-27 23:32:39 +0300

    комментирование

--- public/js/game.js
+++ public/js/game.js
@@ -9,22 +9,29 @@ let tileSize = 32;
 let scale = 1;
 
 function resizeCanvas() {
+    // CSS размер (в логических пикселях)
     const width = window.innerWidth;
     const height = window.innerHeight;
+
+    // Pixel Ratio (для Retina)
     const dpr = window.devicePixelRatio || 1;
 
+    // Устанавливаем реальный размер canvas
     canvas.width = width * dpr;
     canvas.height = height * dpr;
 
+    // Визуальный размер canvas
     canvas.style.width = width + "px";
     canvas.style.height = height + "px";
 
+    // Масштабируем контекст
     ctx.scale(dpr, dpr);
 
     onResize(width, height);
 }
 
 function onResize(width, height) {
+    // Автоматически рассчитываем масштабир. для всех устройств
     if (width < 600) scale = 0.7;
     else if (width < 1200) scale = 1;
     else scale = 1.5;
@@ -44,6 +51,7 @@ resizeCanvas();
 function loop() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
+    // Пример: рисуем кружок в центре экрана
     ctx.fillStyle = "green";
     ctx.beginPath();
     ctx.arc(window.innerWidth/2, window.innerHeight/2, 40, 0, Math.PI*2);
